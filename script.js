183de0e8983d6314893ca714901106f38bda6d5b
Eecodev <mnasser.bbk@gmail.com>
2023-10-30 17:13:31 +0100

    activeIndex added to img title and text

--- script.js
+++ script.js
@@ -55,7 +55,8 @@ const {createApp} = Vue;
                         title: "Marvel's Avengers",
                         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                     },
-                ]
+                ],
+                activeIndex: 0
             }
         },
         methods: {
